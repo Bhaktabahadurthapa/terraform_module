eae1cbd4504850cb9c209de1d50e632abee333fc
Bhakta Thapa <132217155+Bhaktabahadurthapa@users.noreply.github.com>
2024-11-20 14:20:21 -0800

    Create variable.tf

---
+++ module/myproject/variable.tf
@@ -0,0 +1,24 @@
+variable "ami_value" {
+    
+    description = "this is my ami value"
+    type = string
+}
+
+variable "instance_type" {
+
+    description = "this is my instance type"
+    type = string
+  
+}
+variable "instance_count" {
+
+    description = "this is my instance number"
+    type = number
+  
+}
+variable "my_env" {
+
+    description = "this is my environment"
+    type = string
+  
+}
